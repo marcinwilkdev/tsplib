38e64a7f76d44afafe79c805a1979a992171bf2f
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-03-16 17:38:31 +0100

    fix full_matrix not parsing some files properly and refactor a little bit

--- src/tsp_parser/full_matrix.rs
+++ src/tsp_parser/full_matrix.rs
@@ -6,40 +6,35 @@ pub struct FullMatrixTspParser;
 
 impl FullMatrixTspParser {
     pub fn parse(file_lines: &mut Lines, dimension: usize) -> Option<Tsp> {
-        file_lines.next();
-
         let edges = FullMatrixTspParser::parse_lines_into_edges(file_lines, dimension)?;
 
         Some(Tsp { edges })
     }
 
+    // TODO: Refactor this into smaller pieces.
     fn parse_lines_into_edges(file_lines: &mut Lines, dimension: usize) -> Option<Vec<Vec<u32>>> {
         let mut edges = Vec::new();
+
+        let mut curr_line = file_lines.next()?;
+
+        let mut line_weights = curr_line.split_whitespace();
 
         for _ in 0..dimension {
             let mut curr_edges = Vec::new();
 
             let mut edge_counter = 0;
 
-            let mut line = file_lines.next()?;
-
-            let mut split_line = line.split_whitespace();
-
             while edge_counter < dimension {
-                match split_line.next() {
+                match line_weights.next() {
                     Some(edge) => {
-                        let mut edge = edge.parse().ok()?;
-
-                        if edge == 9999 {
-                            edge = 0;
-                        }
+                        let edge = edge.parse().ok()?;
 
                         curr_edges.push(edge);
                         edge_counter += 1;
                     }
                     None => {
-                        line = file_lines.next()?;
-                        split_line = line.split_whitespace();
+                        curr_line = file_lines.next()?;
+                        line_weights = curr_line.split_whitespace();
                     }
                 }
             }
@@ -57,15 +52,13 @@ mod tests {
 
     #[test]
     fn full_matrix_parser_working() {
-        let data = "EDGE_WEIGHT_SECTION
- 9999    2    3
+        let data = "9999    2    3
     2 9999    3
-    3    3 9999
-EOF";
+    3    3 9999";
 
         let mut data_lines = data.lines();
 
         let tsp = FullMatrixTspParser::parse(&mut data_lines, 3).expect("error while parsing data");
-        assert_eq!(vec![vec![0, 2, 3], vec![2, 0, 3], vec![3, 3, 0]], tsp.edges);
+        assert_eq!(vec![vec![9999, 2, 3], vec![2, 9999, 3], vec![3, 3, 9999]], tsp.edges);
     }
 }
